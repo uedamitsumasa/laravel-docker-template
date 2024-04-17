f2b99c016f306cb0ed33048ab48ca559a05cc2a7
uedamitsumasa <unjan19940927@gmail.com>
2024-04-17 12:11:33 +0900

    add-バリデーション

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
 use App\Todo;
-use Illuminate\Http\Request;
 
 
 class TodoController extends Controller
@@ -21,7 +21,7 @@ class TodoController extends Controller
         return view('todo.create');
     }
 
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $inputs = $request->all();
         $this->todo->fill($inputs);
@@ -51,15 +51,14 @@ class TodoController extends Controller
     }
 
 
-    public function update(Request $request, $id)
+    public function update(TodoRequest $request, $id)
     {
         $inputs = $request->all();
         $todo = $this->todo->find($id);
         $todo->fill($inputs);
         $todo->save();
         return redirect()->route('todo.show', $todo->id);
-        
-}
+    }
 
 }
 
